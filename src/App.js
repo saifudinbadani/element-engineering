62196960d5283735298487eb4f8e0746e27d7e5a
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2023-03-21 14:50:55 +0800

    Chore: rendering post on homepage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React from 'react';
-import { Button } from '@mui/material';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Container, AppBar, Toolbar, Typography, Grid, Card, CardContent, CardActions, Button } from '@mui/material';
+import axios from 'axios';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => setPosts(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button variant="contained" color="primary">
-          Hello, Material-UI!
-        </Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">My Blog</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="lg" sx={{ paddingTop: '2rem' }}>
+        <Grid container spacing={3}>
+          {posts.map((post) => (
+            <Grid item key={post.id} xs={12} sm={6} md={4}>
+              <Card>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {post.title}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    {post.body}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    View Post
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </div>
   );
 }
